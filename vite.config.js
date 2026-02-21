4c095ae8c9d3d6d3f095d98b031154cd7ec1198c
Jovany Cardoza-Aguilar <jovanycardozaaguilar@gmail.com>
2026-02-20 17:49:00 -0800

    Updated vite config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,18 @@
 import { defineConfig } from 'vite';
 import tailwindcss from '@tailwindcss/vite';
+import { resolve } from 'path';
 
 export default defineConfig({
   plugins: [tailwindcss()],
-  base: '/jovanycardozaaguilar-tailwind-vite-website/',
+  base: '/',  
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        blog: resolve(__dirname, 'blog.html'),
+        projects: resolve(__dirname, 'projects.html'),
+        publications: resolve(__dirname, 'publications.html')
+      }
+    }
+  }
 });
