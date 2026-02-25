21f9b08f6be6b26d22ba5c2821d07a462948b27d
Jovany Cardoza-Aguilar <jovanycardozaaguilar@gmail.com>
2026-02-24 22:44:23 -0800

    Fixed blog posts not showing up

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,20 @@
 import { defineConfig } from 'vite';
 import tailwindcss from '@tailwindcss/vite';
 import { resolve } from 'path';
+import fg from 'fast-glob';
 
 export default defineConfig({
   plugins: [tailwindcss()],
   base: '/',
   build: {
     rollupOptions: {
-      input: {
-        main: resolve(__dirname, 'index.html'),
-        blog: resolve(__dirname, 'blog.html'),
-        portfolio: resolve(__dirname, 'portfolio.html'),
-      }
+      input: Object.fromEntries(
+        fg.sync(['**/*.html'], { ignore: ['dist/**', 'node_modules/**'] }).map(file => [
+          // This creates a key like "blog/post1" from "blog/post1.html"
+          file.slice(0, -5).replace(/\\/g, '/'),
+          resolve(__dirname, file)
+        ])
+      )
     }
   }
 });
