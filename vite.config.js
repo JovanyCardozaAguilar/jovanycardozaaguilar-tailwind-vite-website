bfa18d7ac5b815ce02dda1911c843e8d76be780b
Jovany Cardoza-Aguilar <jovanycardozaaguilar@gmail.com>
2026-02-24 22:19:39 -0800

    Changes to config and blog htmls

--- vite.config.js
+++ vite.config.js
@@ -10,8 +10,7 @@ export default defineConfig({
       input: {
         main: resolve(__dirname, 'index.html'),
         blog: resolve(__dirname, 'blog.html'),
-        projects: resolve(__dirname, 'projects.html'),
-        publications: resolve(__dirname, 'publications.html')
+        projects: resolve(__dirname, 'portfolio.html'),
       }
     }
   }
